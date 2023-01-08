57ef11b67f4d722b5e46039142b0cb42e83e4f70
polak <polak.barel@gmail.com>
2023-01-08 03:59:27 +0200

    blabla

---
+++ lesson 6/HW-B2/ex1.py
@@ -0,0 +1,22 @@
+import pprint
+
+
+def remove_empty(original_dict: dict, inplace=False) -> dict:
+    second_dict = {}
+    count = 0
+    for key in sorted(original_dict, reverse=True):
+        if inplace is True:
+            count = 1
+            if original_dict[key] != [] and original_dict[key] != () and original_dict[key] != '' and original_dict[key] != None:
+                second_dict[key] = original_dict[key]
+        else:
+            if original_dict[key] == [] or original_dict[key] == () or original_dict[key] == '' or original_dict[key] == {} or original_dict[key] == None:
+                original_dict.pop(key)
+    if count == 1:
+        return second_dict
+    else:
+        return original_dict
+
+
+a = {'c1': 'Red', 'c2': 'Green', 'c3': None, 'c4': [], 'c5': ''}
+pprint.pprint(remove_empty(a))
